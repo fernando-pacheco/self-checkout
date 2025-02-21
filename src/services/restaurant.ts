7b0c02bcd4319149ef05a6063ead4a79820424d8
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-02-21 17:29:20 -0300

    feat: retorno de categorias e produtos para restaurante selecionado

--- src/services/restaurant.ts
+++ src/services/restaurant.ts
@@ -3,6 +3,7 @@ import { db } from "@/lib/prisma"
 async function getRestaurantBySlug(slug: string) {
     return await db.restaurant.findUnique({
         where: { slug },
+        include: { MenuCategory: { include: { Product: true } } },
     })
 }
 
