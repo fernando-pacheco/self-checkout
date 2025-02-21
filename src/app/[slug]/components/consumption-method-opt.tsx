acd77c32bd69f22b6dd8ce9be6556e15ba3aa4cc
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-02-21 16:14:54 -0300

    feat: uso do useRouter para realizar a navegação entre as páginas

--- src/app/[slug]/components/consumption-method-opt.tsx
+++ src/app/[slug]/components/consumption-method-opt.tsx
@@ -1,20 +1,37 @@
+"use client"
+
 import { Button } from "@/components/button"
 import { CardContent } from "@/components/card"
+import type { ConsumptionMethod } from "@prisma/client"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 interface ConsumptionMethodOptProps {
     buttonText: string
     imageUrl: string
     imageAlt: string
+    option: ConsumptionMethod
+    slug: string
 }
 
-function ConsumptionMethodOpt(props: ConsumptionMethodOptProps) {
+function ConsumptionMethodOpt({
+    buttonText,
+    imageAlt,
+    imageUrl,
+    option,
+    slug,
+}: ConsumptionMethodOptProps) {
+    const router = useRouter()
+    function handleNavigate() {
+        router.push(`/${slug}/menu?consumptionMethod=${option}`)
+    }
+
     return (
         <CardContent>
             <div className="relative w-[80px] h-[80px]">
-                <Image src={props.imageUrl} fill alt={props.imageAlt} />
+                <Image src={imageUrl} fill alt={imageAlt} />
             </div>
-            <Button>{props.buttonText}</Button>
+            <Button onClick={handleNavigate}>{buttonText}</Button>
         </CardContent>
     )
 }
