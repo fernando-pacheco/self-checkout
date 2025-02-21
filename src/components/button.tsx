a32d04f24e1b5a8b81a3e722908529cecaa99a82
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-02-21 16:13:49 -0300

    feat: estilo do botão de rounded-full para lg

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,7 +7,7 @@ function Button({ className, ...props }: ButtonProps) {
     return (
         <button
             className={twMerge(
-                "rounded-full bg-gray-100 p-2 cursor-pointer hover:bg-gray-200 hover:text-white transition-colors duration-150",
+                "rounded-lg bg-gray-100 p-2 cursor-pointer hover:bg-gray-200 hover:text-white transition-colors duration-150",
                 className
             )}
             {...props}
