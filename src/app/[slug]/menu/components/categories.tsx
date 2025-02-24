59b1f02d0d62fdf54840fec0880cf7d51987e855
fernando-pacheco <fernandopachecopx@gmail.com>
2025-02-24 20:44:08 -0300

    refactor: uso de uma scroll area para navegação de catagorias

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -2,10 +2,9 @@
 
 import { Button } from "@/components/button"
 import { ScrollArea, ScrollBar } from "@/components/scroll-area"
-import type { MenuCategory, Prisma, Restaurant } from "@prisma/client"
-import { Clock, Star } from "lucide-react"
-import Image from "next/image"
+import type { Prisma } from "@prisma/client"
 import { useState } from "react"
+import { RestaurantProducts } from "./products"
 
 interface RestaurantCategoriesProps {
     restaurant: Prisma.RestaurantGetPayload<{
@@ -13,46 +12,25 @@ interface RestaurantCategoriesProps {
     }>
 }
 
-function RestaurantCategories({ restaurant }: RestaurantCategoriesProps) {
-    const [selectedCategory, setSelectedCategory] = useState<MenuCategory>(
-        restaurant.MenuCategory[0]
-    )
+type MenuCategoryProducts = Prisma.MenuCategoryGetPayload<{
+    include: { Product: true }
+}>
 
-    function handleCategoryClick(category: MenuCategory) {
+function RestaurantCategories({ restaurant }: RestaurantCategoriesProps) {
+    const [selectedCategory, setSelectedCategory] =
+        useState<MenuCategoryProducts>(restaurant.MenuCategory[0])
+
+    function handleCategoryClick(category: MenuCategoryProducts) {
         setSelectedCategory(category)
     }
 
+    function getCategoryButtonVariant(category: MenuCategoryProducts) {
+        return selectedCategory.id === category.id ? "secondary" : "default"
+    }
+
     return (
-        <div>
-            <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border border-y-gray-700/30 border-x-0 bg-white p-5 space-y-3">
-                <div className="flex justify-between items-center">
-                    <div className="flex items-center gap-3">
-                        <Image
-                            src={restaurant.avatarImage}
-                            alt={restaurant.name}
-                            width={45}
-                            height={45}
-                        />
-                        <div>
-                            <h2 className="text-lg font-semibold">
-                                {restaurant.name}
-                            </h2>
-                            <p className="text-xs text-gray-200">
-                                {restaurant.description}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="border border-gray-200/60 px-3 py-1 flex gap-1 items-center text-sm rounded-full font-semibold">
-                        <Star className="fill-yellow text-yellow size-3.5" />
-                        <p>5.0</p>
-                    </div>
-                </div>
-                <div className="text-limegreen flex gap-1 text-sm items-center">
-                    <Clock className="size-4" />
-                    <p>Aberto!</p>
-                </div>
-            </div>
-            <ScrollArea className="w-full ">
+        <div className="space-y-2">
+            <ScrollArea className="w-full">
                 <ScrollBar orientation="horizontal" />
                 <div className="flex w-max space-x-4 p-4">
                     {restaurant.MenuCategory.map((category) => (
@@ -60,17 +38,15 @@ function RestaurantCategories({ restaurant }: RestaurantCategoriesProps) {
                             key={category.id}
                             className="rounded-full py-1 text-base"
                             onClick={() => handleCategoryClick(category)}
-                            variant={
-                                selectedCategory.id === category.id
-                                    ? "secondary"
-                                    : "default"
-                            }
+                            variant={getCategoryButtonVariant(category)}
                         >
                             {category.name}
                         </Button>
                     ))}
                 </div>
             </ScrollArea>
+            <h1 className="px-4 font-semibold">{selectedCategory.name}</h1>
+            <RestaurantProducts products={selectedCategory.Product} />
         </div>
     )
 }
