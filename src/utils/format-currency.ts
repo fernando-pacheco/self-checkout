542cb8eac4fb179d123f596c0b91aaf95ee57ea9
fernando-pacheco <fernandopachecopx@gmail.com>
2025-02-24 20:41:07 -0300

    feat: função utilitária para formatação de moeda

---
+++ src/utils/format-currency.ts
@@ -0,0 +1,8 @@
+function formatCurrency(value: number) {
+    return Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(value)
+}
+
+export { formatCurrency }
