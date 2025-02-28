677e818346c9e161f96ab60aeac234a1519710cf
fernando-pacheco <fernandopachecopx@gmail.com>
2025-02-27 21:06:54 -0300

    feat: uso dos tooltips para melhorar ux

--- src/components/molecules/header-navigation.tsx
+++ src/components/molecules/header-navigation.tsx
@@ -2,8 +2,14 @@
 
 import { CartSheet } from "@/components/organisms/cart-sheet"
 import { useCart } from "@/hooks/use-cart"
-import { ChevronLeft, ScrollText } from "lucide-react"
+import { ChevronLeft, ScrollText, ShoppingCart } from "lucide-react"
 import { Button } from "../atoms/button"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "../atoms/tooltip"
 
 interface HeaderNavigationProps {
     navigation: VoidFunction
@@ -18,15 +24,56 @@ function HeaderNavigation({ navigation }: HeaderNavigationProps) {
 
     return (
         <div>
-            <Button onClick={navigation} className="absolute top-4 left-4 z-50">
-                <ChevronLeft className="size-5" />
-            </Button>
-            <Button
-                className="absolute top-4 right-4 z-50"
-                onClick={handleOpenCart}
-            >
-                <ScrollText className="size-5" />
-            </Button>
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button
+                            onClick={navigation}
+                            className="absolute top-4 left-4 z-50"
+                        >
+                            <ChevronLeft className="size-5" />
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent
+                        side="right"
+                        className="font-semibold px-4 text-white"
+                    >
+                        <p>Voltar</p>
+                    </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button
+                            className="absolute top-4 right-4 z-50"
+                            onClick={handleOpenCart}
+                        >
+                            <ScrollText className="size-5" />
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent
+                        side="left"
+                        className="font-semibold px-4 text-white"
+                    >
+                        <p>Pedidos</p>
+                    </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button
+                            className="absolute top-16 right-4 z-50"
+                            onClick={handleOpenCart}
+                        >
+                            <ShoppingCart className="size-5" />
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent
+                        side="left"
+                        className="font-semibold px-4 text-white"
+                    >
+                        <p>Carrinho</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
             <CartSheet />
         </div>
     )
